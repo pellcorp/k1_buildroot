24774a64fcc16461a5841c86727886fc911a2e31
Jason Pell <jason@pellcorp.com>
2024-06-07 20:52:25 +1000

    produce .tar files not tgz files

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -173,8 +173,8 @@ package_variant() {
 	variant_env $1
 	rm -f "$br_chroot"
 	xz -cT`nproc` "$br_image" > "$br_chroot"
-	package_name="K1-KlipperMod-$GIT_VERSION-$variant.tgz"
-	package_name_pro="K1Max-KlipperMod-$GIT_VERSION-$variant.tgz"
+	package_name="K1-KlipperMod-$GIT_VERSION-$variant.tar"
+	package_name_pro="K1Max-KlipperMod-$GIT_VERSION-$variant.tar"
 	mkdir -p $BUILD_PACKAGE
 	tar -cf "$BUILD_PACKAGE/$package_name" -C "$GIT_ROOT/device_files/install" . -C "$br_builddir/images/" ./chroot.tar.xz
 	cp "$BUILD_PACKAGE/$package_name" "$BUILD_PACKAGE/$package_name_pro"
@@ -187,7 +187,7 @@ package_variant() {
 
 package_uninstall()
 {
-	package_name="K1-KlipperMod-uninstall.tgz"
+	package_name="K1-KlipperMod-uninstall.tar"
 	tar -cf "$BUILD_PACKAGE/$package_name" -C "$GIT_ROOT/device_files/uninstall" .
 	log_info "uninstall: created $package_name"
 }
