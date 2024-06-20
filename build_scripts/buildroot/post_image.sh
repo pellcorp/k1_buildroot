3d4799e8a5db9896afccb9cde4d54e4d3188f415
Jason Pell <jason@pellcorp.com>
2024-06-20 17:57:47 +1000

    ok cleanup the image before generating

--- build_scripts/buildroot/post_image.sh
+++ build_scripts/buildroot/post_image.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/../env.sh
@@ -8,7 +7,10 @@ source $BASE_DIR/.mod_env
 export VARIANT_BUILDROOT_OUT=$BUILDROOT_OUT/$MOD_VARIANT
 
 if [ "$MOD_VARIANT" = "simple" ] && [ -f $VARIANT_BUILDROOT_OUT/images/rootfs.tar ]; then
-	echo "Deleting unecessary files from skeleton..."
-	#tar -f $VARIANT_BUILDROOT_OUT/images/rootfs.tar --delete ./dev ./etc ./media ./mnt ./opt ./proc ./root ./run ./sys ./tmp ./var
+	echo "Deleting unecessary dirs from rootfs.tar ..."
+	tar -f $VARIANT_BUILDROOT_OUT/images/rootfs.tar --delete ./dev ./etc ./media ./mnt ./opt ./proc ./root ./run ./sys ./tmp ./var
+    [ -f $VARIANT_BUILDROOT_OUT/images/rootfs.tar.gz ] && rm $VARIANT_BUILDROOT_OUT/images/rootfs.tar.gz
+
+    echo "Gzipping rootfs.tar ..."
 	gzip -k $VARIANT_BUILDROOT_OUT/images/rootfs.tar 
 fi
